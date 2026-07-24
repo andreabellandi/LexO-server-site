60ffe8c052ca5617a30e6b4527b37b6ed037699d
Andrea Bellandi <7847238+andreabellandi@users.noreply.github.com>
2026-07-24 11:19:28 +0200

    Add project icons

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,21 +23,35 @@ $(document).ready(function () {
         }
     });
 
-    const $vocaboProject = $('#projects .project-row').first();
-    $vocaboProject.find('.project-icon')
-        .attr({
-            src: 'assets/images/vocabo-icon.png',
-            alt: 'VocaBO project icon'
-        })
-        .css({
-            width: '70px',
-            height: '70px',
-            padding: '4px',
-            boxSizing: 'border-box',
-            objectFit: 'contain',
-            alignSelf: 'center'
-        });
-    $vocaboProject.find('.project-title-col').css('align-self', 'center');
+    const projectIcons = [
+        { file: 'vocabo-icon.png', alt: 'VocaBO project icon' },
+        { file: 'ownw-icon.jpg', alt: 'OWNW project icon' },
+        { file: 'itant-icon.jpg', alt: 'ItAnt project icon' },
+        { file: 'ditmao-icon.jpg', alt: 'DiTMAO project icon' }
+    ];
+
+    $('#projects .project-row').each(function (index) {
+        const icon = projectIcons[index];
+        if (!icon) {
+            return;
+        }
+
+        $(this).find('.project-icon')
+            .attr({
+                src: `assets/images/${icon.file}`,
+                alt: icon.alt
+            })
+            .css({
+                width: '70px',
+                height: '70px',
+                padding: '4px',
+                boxSizing: 'border-box',
+                objectFit: 'contain',
+                alignSelf: 'center'
+            });
+
+        $(this).find('.project-title-col').css('align-self', 'center');
+    });
 });
 
 function navigate(sectionId) {
